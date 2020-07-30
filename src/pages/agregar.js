0ffb327770e80f42c4b4e50a630411753ad87155
maguiceri <magui.cerisola@gmail.com>
2020-07-30 12:41:35 -0700

    pages

--- src/pages/agregar.js
+++ src/pages/agregar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
 
-
 class Agregar extends React.Component {
     constructor(props) {
         super(props);
@@ -19,7 +18,6 @@ class Agregar extends React.Component {
 
     handleChange(e) {
         const {name, value} = e.target;
-
         this.setState({
             [name]: value //actualiza una variable
         })
@@ -38,23 +36,18 @@ class Agregar extends React.Component {
     render() {
 
         return(
-
             <>
-
             <Navbar />
-
             <div>
                 <input type="text" name="name" placeholder="Nombre" onChange={(e) => this.handleChange(e)} />
                 <input type="text" name="logo" placeholder="Logo" onChange={(e) => this.handleChange(e)}/>
                 <input type="text" name="email" placeholder="email" onChange={(e) => this.handleChange(e)}/>
                 <input type="text" name="desc" placeholder="description" onChange={(e) => this.handleChange(e)}/>
+                <input type="text" name="web" placeholder="Web (opcional)" onChange={(e) => this.handleChange(e)}/>
                 <input type="text" name="tw" placeholder="Twitter (opcional)" onChange={(e) => this.handleChange(e)}/>
                 <input type="text" name="ig" placeholder="Instagram (opcional)" onChange={(e) => this.handleChange(e)}/>
                 <button className="boton-agregar" onClick={() => this.handleClick()}>enviar</button>
-                
-   
-            </div>
-            
+            </div> 
             </>
         )
     }
