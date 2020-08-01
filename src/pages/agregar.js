6946d1435e958fe4639963bafd1999469ad4cccc
maguiceri <magui.cerisola@gmail.com>
2020-07-31 19:35:44 -0700

    state

--- src/pages/agregar.js
+++ src/pages/agregar.js
@@ -12,12 +12,14 @@ class Agregar extends React.Component {
             desc: "",
             web:"",
             tw:"",
-            ig: ""
+            ig: "",
+            esta0k:"",
+            showMessage: false
         }
     }
 
     handleChange(e) {
-        const {name, value} = e.target;
+        const {name, value} = e.target; 
         this.setState({
             [name]: value //actualiza una variable
         })
@@ -25,20 +27,39 @@ class Agregar extends React.Component {
 
     handleClick() {
         const {name, logo, email, desc} = this.state;
+        const isValid = name && email && logo && desc ? true : false; //replico el if pero en un ternario donde me guardo en la variable isValid dependiendo el rdo.
         
-        if(name && logo && email && desc) {  //existencia
-            console.log('TODO ESTA BIEN');
-        } else {
-            console.log('TODO ESTA MAL');
-        }
+        this.setState({
+            estaOk: isValid,
+            showMessage: true //porque esto siempre es true, reemplace en 5 lines, 11
+        })
+      
+        //  if(name && logo && email && desc) {  //existencia
+      //      this.setState({
+      //         estaOk: true,
+    //         showMessage: true
+      //      });
+
+      //  } else {
+      //      this.setState({
+       //         estaOk: false,
+        //        showMessage: true
+        //    });
+
+     //   }
     }
 
     render() {
-
+        const {estaOk, showMessage} = this.state
         return(
             <>
             <Navbar />
             <div>
+
+                {showMessage && (
+                <p>{estaOk ? ("Todo salio bien") : "todo salio mal"}</p>
+                )}
+
                 <input type="text" name="name" placeholder="Nombre" onChange={(e) => this.handleChange(e)} />
                 <input type="text" name="logo" placeholder="Logo" onChange={(e) => this.handleChange(e)}/>
                 <input type="text" name="email" placeholder="email" onChange={(e) => this.handleChange(e)}/>
